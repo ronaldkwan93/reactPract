6a2ce710176230644dacc148ed52f4b5f64fa723
Ron Kwan <132134496+ronaldkwan93@users.noreply.github.com>
2024-05-08 23:22:09 +1000

    Created React app

---
+++ practice-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <h1>
+          Hello World!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
